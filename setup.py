31eb42ee3179af2a81c777218120966edd6533af
madkerpe <marijn.dekerpel@ugent.be>
2021-06-24 17:26:57 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
 setup(
     name="minine",
     version="0.3",
     author="Marijn De Kerpel",
     author_email="marijn.dekerpel@gmail.com",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=requirements,
     package_data={
         "minine": ["minine/modules/config/*", "minine/modules/weights/*"],
     },
